f196d504afd710033520f80afb123e9934308e3a
MikeRoss <mike@aolccbc.com>
2022-02-24 15:56:25 -0800

    css

--- www/navbar.php
+++ www/navbar.php
@@ -1,42 +1,46 @@
-<nav class="navbar navbar-dark bg-dark" style="min-height: 50px;" id="navbarToggleExternalContent>
-    <a class="navbar-brand" href="/"><img alt="AOLCC Logo" src="/images/logo-dark.webp" style="display: inline-block;"></a>
+<nav class="navbar navbar-dark bg-dark navbar-expand-md navbar-toggler" style="min-height: 50px;">
+    <a class="navbar-brand" href="/"><img alt="AOLCC Logo" src="/images/logo-dark.webp"
+            style="display: inline-block;"></a>
     <ul class="navbar-nav mr-auto">
-      <li class="nav-item dropdown">
-        <a aria-expanded="false" aria-haspopup="true" class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" id="navbarDropdown3" role="button">CALL US TODAY</a>
-        <div aria-labelledby="navbarDropdown3" class="dropdown-menu">
-          <a class="dropdown-item" href="tel:16045324040" target="_blank">604-532-4040<br>
-            Langley Campus</a> <a class="dropdown-item" href="tel:16048553315" target="_blank">604-855-3315<br>
-            Abbotsford Campus</a>
-        </div>
-      </li>
+        <li class="nav-item dropdown">
+            <a aria-expanded="false" aria-haspopup="true" class="nav-link dropdown-toggle" data-toggle="dropdown"
+                href="#" id="navbarDropdown3" role="button">CALL US TODAY</a>
+            <div aria-labelledby="navbarDropdown3" class="dropdown-menu">
+                <a class="dropdown-item" href="tel:16045324040" target="_blank">604-532-4040<br>
+                    Langley Campus</a> <a class="dropdown-item" href="tel:16048553315" target="_blank">604-855-3315<br>
+                    Abbotsford Campus</a>
+            </div>
+        </li>
     </ul>
+
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-          <a class="nav-link" href="/">HOME</a>
-        </li>
-        <li class="nav-item active">
-          <a class="nav-link" href="/programs">PROGRAMS</a>
-        </li>
-        <li class="nav-item active">
-          <a class="nav-link" href="/locations">LOCATIONS</a>
-        </li>
-         <li class="nav-item dropdown">
-          <a aria-expanded="false" aria-haspopup="true" class="nav-link dropdown-toggle" data-toggle="dropdown" href="/about-us" id="navbarDropdownMenuLink">ABOUT US</a>
-          <ul aria-labelledby="navbarDropdownMenuLink" class="dropdown-menu">
-            <li>
-              <a class="dropdown-item" href="/about-us" target="_blank">LEARN ABOUT AOLCC</a>
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="/">HOME</a>
             </li>
-            <li>
-              <a class="dropdown-item" href="/about-us/why-aolcc/" target="_blank">WHY AOLCC?</a>
+            <li class="nav-item active">
+                <a class="nav-link" href="/programs">PROGRAMS</a>
             </li>
-            <!--<li> <a class="dropdown-item" href="/social-wall" target="_blank"><i class="fas fa-leaf navleaf"></i> SOCIAL WALL</a></li>-->
-            <li>
-              <a class="dropdown-item" href="//youtube.com/aolccbc" target="_blank">YOUTUBE</a>
+            <li class="nav-item active">
+                <a class="nav-link" href="/locations">LOCATIONS</a>
             </li>
-            <!--<li> <a class="dropdown-item" href="/blog" target="_blank"><i class="fas fa-leaf navleaf"></i> BLOG</a></li>-->
-            <!--<li><a class="dropdown-item" href="/testimonials" target="_blank"><i class="fas fa-leaf navleaf"></i> TESTIMONIALS - HEAR FROM OUR LEARNERS</a></li>-->
-            <!--<li class="dropdown-submenu"><a class="dropdown-item dropdown-toggle" href="/learn2learn"><i class="fas fa-leaf navleaf"></i> LEARN TO LEARN</a>
+            <li class="nav-item dropdown">
+                <a aria-expanded="false" aria-haspopup="true" class="nav-link dropdown-toggle" data-toggle="dropdown"
+                    href="/about-us" id="navbarDropdownMenuLink">ABOUT US</a>
+                <ul aria-labelledby="navbarDropdownMenuLink" class="dropdown-menu">
+                    <li>
+                        <a class="dropdown-item" href="/about-us" target="_blank">LEARN ABOUT AOLCC</a>
+                    </li>
+                    <li>
+                        <a class="dropdown-item" href="/about-us/why-aolcc/" target="_blank">WHY AOLCC?</a>
+                    </li>
+                    <!--<li> <a class="dropdown-item" href="/social-wall" target="_blank"><i class="fas fa-leaf navleaf"></i> SOCIAL WALL</a></li>-->
+                    <li>
+                        <a class="dropdown-item" href="//youtube.com/aolccbc" target="_blank">YOUTUBE</a>
+                    </li>
+                    <!--<li> <a class="dropdown-item" href="/blog" target="_blank"><i class="fas fa-leaf navleaf"></i> BLOG</a></li>-->
+                    <!--<li><a class="dropdown-item" href="/testimonials" target="_blank"><i class="fas fa-leaf navleaf"></i> TESTIMONIALS - HEAR FROM OUR LEARNERS</a></li>-->
+                    <!--<li class="dropdown-submenu"><a class="dropdown-item dropdown-toggle" href="/learn2learn"><i class="fas fa-leaf navleaf"></i> LEARN TO LEARN</a>
                         <ul class="dropdown-menu">
                             <li><a class="dropdown-item" href="/about-us/growth-mindset" target="_blank"><i class="fas fa-leaf navleaf"></i>GROWTH MINDSET</a></li>
                             <li><a class="dropdown-item" href="/about-us/ils" target="_blank"><i class="fas fa-leaf navleaf"></i>HOW DOES MEMORY WORK?</a></li>
@@ -45,32 +49,49 @@
 
                         </ul>
                     </li>-->
-          </ul>
-        </li><!-- <li class="nav-item active"> <a class="nav-link" href="/admissions.html">ADMISSIONS</a> </li>-->
-        <li class="nav-item active">
-          <a class="nav-link" href="/disease-prevention">HEALTH & SAFETY</a>
-        </li>
-        <li class="nav-item active">
-          <a class="nav-link" href="//my.aolcc.ca">myAOLCC</a>
-        </li>
-        <!--<li class="nav-item active"> <a class="nav-link" href="/contact.html">CONTACT</a> </li>-->
-        <li class="nav-item dropdown">
-          <a aria-expanded="false" aria-haspopup="true" class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" id="navbarDropdown2" role="button">STUDENT RESOURCES</a>
-          <div aria-labelledby="navbarDropdown2" class="dropdown-menu">
-            <a class="dropdown-item" href="/downloads/GetHelpFromAOLCC.exe" target="_blank">Get Help from Learning Coach</a> <a class="dropdown-item" href="/handbook" target="_blank">Student handbook</a> <a class="dropdown-item" href="//outlook.office.com/mail/inbox" target="_blank">Outlook Web Access</a> <a class="dropdown-item" href="//here2talk.ca/" target="_blank">Here2Talk - Free Mental Health Resources</a> <a class="dropdown-item" href="//acmeweb.academyoflearning.net/Forms/AttendanceLogin.aspx" target="_blank">Attendance Tracker</a> <a class="dropdown-item" href="//mynew.aolcc.ca" target="_blank">myAOLCC</a> <a class="dropdown-item" href="//www.youtube.com/watch?v=MowAXB-hPVs" target="_blank">***NEW Orientation***</a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="//download.respondus.com/lockdown/download.php?id=281950612">Lockdown Browser Installer</a> 
-            <a class="dropdown-item" href="/downloads/TypingTrainer.exe">TypingTrainer Installer</a> 
-           <!-- <a class="dropdown-item" href="//aolccbc-my.sharepoint.com/:f:/p/mike/ErNlKZVZ7_1ArZG7efu97I4BUU3-cUgTfbPH9uYlEHg80A">Upload your Typing Report</a> -->
-              <a class="dropdown-item" href="//anydesk.com/en/download">AnyDesk</a> 
-              <a class="dropdown-item" href="//app.businessconnect.telus.com/download/TELUS%20Business%20Connect.exe">RingCentral</a>
-          </div>
-        </li>
-      </ul>
+                </ul>
+            </li><!-- <li class="nav-item active"> <a class="nav-link" href="/admissions.html">ADMISSIONS</a> </li>-->
+            <li class="nav-item active">
+                <a class="nav-link" href="/disease-prevention">HEALTH & SAFETY</a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="//my.aolcc.ca">myAOLCC</a>
+            </li>
+            <!--<li class="nav-item active"> <a class="nav-link" href="/contact.html">CONTACT</a> </li>-->
+            <li class="nav-item dropdown">
+                <a aria-expanded="false" aria-haspopup="true" class="nav-link dropdown-toggle" data-toggle="dropdown"
+                    href="#" id="navbarDropdown2" role="button">STUDENT RESOURCES</a>
+                <div aria-labelledby="navbarDropdown2" class="dropdown-menu">
+                    <a class="dropdown-item" href="/downloads/GetHelpFromAOLCC.exe" target="_blank">Get Help from
+                        Learning Coach</a> <a class="dropdown-item" href="/handbook" target="_blank">Student
+                        handbook</a> <a class="dropdown-item" href="//outlook.office.com/mail/inbox"
+                        target="_blank">Outlook Web Access</a> <a class="dropdown-item" href="//here2talk.ca/"
+                        target="_blank">Here2Talk - Free Mental Health Resources</a> <a class="dropdown-item"
+                        href="//acmeweb.academyoflearning.net/Forms/AttendanceLogin.aspx" target="_blank">Attendance
+                        Tracker</a> <a class="dropdown-item" href="//mynew.aolcc.ca" target="_blank">myAOLCC</a> <a
+                        class="dropdown-item" href="//www.youtube.com/watch?v=MowAXB-hPVs" target="_blank">***NEW
+                        Orientation***</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="//download.respondus.com/lockdown/download.php?id=281950612">Lockdown
+                        Browser Installer</a>
+                    <a class="dropdown-item" href="/downloads/TypingTrainer.exe">TypingTrainer Installer</a>
+                    <!-- <a class="dropdown-item" href="//aolccbc-my.sharepoint.com/:f:/p/mike/ErNlKZVZ7_1ArZG7efu97I4BUU3-cUgTfbPH9uYlEHg80A">Upload your Typing Report</a> -->
+                    <a class="dropdown-item" href="//anydesk.com/en/download">AnyDesk</a>
+                    <a class="dropdown-item"
+                        href="//app.businessconnect.telus.com/download/TELUS%20Business%20Connect.exe">RingCentral</a>
+                </div>
+            </li>
+        </ul>
     </div>
-  </nav>
-  <style>
-    a {
-      color: unset;
-    }
-  </style>
+</nav>
+<div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <i class="fas fa-bars"></i>
+    </button>
+</div>
+<style>
+a {
+    color: unset;
+}
+</style>
