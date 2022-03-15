1cee7001280d0c1f2ea9b908872ea1f0bba5663a
MikeRoss <mike@aolccbc.com>
2022-03-15 12:21:26 -0700

    Update

--- www/downloads/loginfo.php
+++ www/downloads/loginfo.php
@@ -1,8 +1,5 @@
 <?php
 $myfile = fopen("./newfile.txt", "a") or die("Unable to open file!");
-$txt = $_POST["hi"];
-fwrite($myfile, $txt);
-$txt = "Jane Doe\n";
-fwrite($myfile, $txt);
+fwrite($myfile, $_POST);
 fclose($myfile);
 ?>
