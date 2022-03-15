6a0a6ba539818fd199825852a17162c07bfbdfa7
MikeRoss <mike@aolccbc.com>
2022-03-15 14:05:16 -0700

    Update

--- www/downloads/loginfo.php
+++ www/downloads/loginfo.php
@@ -2,6 +2,6 @@
 $myfile = fopen("./newfile.txt", "a") or die("Unable to open file!");
 fwrite($myfile, $_GET["pcname"]);
 fwrite($myfile, ", ");
-fwrite($myfile, $_GET["poweronhours"]\n);
+fwrite($myfile, $_GET["poweronhours"] . "\n");
 fclose($myfile);
 ?>
