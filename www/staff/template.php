74510853a74f417bd2232a76dd201f4eff2f830a
MikeRoss <mike@aolccbc.com>
2022-04-04 11:04:34 -0700

    Update

--- www/staff/template.php
+++ www/staff/template.php
@@ -1,1 +1,127 @@
-Hello World
+<html>
+
+<head>
+
+<link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="wisestamp_app banner" style="margin: 8px 8px 0 0; display: inline-block"> <a
+            href="http://www.academyoflearning.com" target="_blank"> <img moz-do-not-send="true" src="https://s3.amazonaws.com/ucwebapp.wisestamp.com/56cd8643-b36d-
+44ce-a409-7790b45559f9/academy_of_learning_logo.format_png.
+resize_470x.png" height="85" width="250"> </a> </div>
+    <br><br>
+    <div style="max-width:600px;  direction: ltr; " class="main_html
+date__2016_04_15___16_39">
+        <div style="clear:both;height:0!important"></div>
+        <div class="html
+wisestamp_app main_sig" id="new_13" style="max-width:470px;display:
+table;margin: 8px 8px 8px 8px; text-align:initial;">
+            <div style="display: inline-block;">
+                <table cellpadding="0" cellspacing="0">
+                    <tbody>
+                        <tr>
+                            <td colspan="2"><span style="line-height:
+15px;text-align:left;color: #9b929f; font-size:12px; font-family:
+arial, serif; font-weight:bold;">Your Name</span></td>
+                        </tr>
+                        <tr>
+                            <td colspan="2"><span style="line-height:
+15px;text-align:left;color:#9b929f; font-size:12px; font-family:
+arial, serif;">Your Title</span></td>
+                        </tr>
+                        <tr>
+                            <td colspan="2"><span style="line-height:
+15px;text-align:left;color:#ed7500; font-size:12px; font-family:
+arial, serif;">Academy of Learning Career College</span></td>
+                        </tr>
+                        <tr>
+                            <td colspan="2">&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td width="10"> <span style="display:inline-block; width:
+17px;font-size:12px;color:#ed7500">t.</span> </td>
+                            <td valign="bottom">
+                                <div style="line-height:
+15px;text-align:initial;color: #9b929f; font-size:11px; font-family:
+arial, serif;"> <a target="_blank" href="tel:1-855-996-9977 x 2160" style="color: #9b929f; text-decoration:none;
+font-size:12px;">1-855-996-9977</a> </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td> <span style="display:inline-block; width:
+17px;font-size:12px;color:#ed7500">m.</span> </td>
+                            <td valign="bottom">
+                                <div style="line-height:
+15px;text-align:initial;color: #9b929f; font-size:11px; font-family:
+arial, serif;"><a target="_blank" href="tel:1-905-441-5190" style="color: #9b929f; text-decoration:none;
+font-size:12px;">Your Mobile #</a></div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td> <span style="display:inline-block; width:
+17px;font-size:12px;color:#ed7500">e.</span> </td>
+                            <td valign="bottom">
+                                <div style="line-height:
+15px;text-align:initial;color: #9b929f; font-size:12px; font-family:
+arial, serif;"><a target="_blank" href="mailto:" style="color: #9b929f;
+text-decoration:none;">your email</a></div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td> <span style="display:inline-block; width: 17px;
+font-size:12px; color:#ed7500">w.</span> </td>
+                            <td valign="bottom">
+                                <div style="line-height: 15px;text-align:initial;color: #9b929f;
+font-size:12px; font-family: arial, serif;"><a target="_blank" href="http://www.academyoflearning.com" style="color: #9b929f;
+text-decoration:none;">www.academyoflearning.com</a></div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td> <span style="display:inline-block; width: 17px;
+font-size:12px; color:#ed7500">a.</span> </td>
+                            <td valign="bottom">
+                                <div style="line-height: 15px;text-align:initial;color: #9b929f;
+font-size:12px; font-family: arial, serif;">100 York Boulevard, Suite 400,
+                                    Richmond Hill, ON, L4B 1J8</div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td colspan="2" style="border-bottom:2px solid #ed7500; padding-top: 7px;"></td>
+                        </tr>
+                        <tr>
+                            <td colspan="2" style="padding-top:9px;
+text-align:initial;">
+                                <div><a href="http://www.facebook.com/academyoflearning/" target="_blank"><img
+                                            moz-do-not-send="true" style="border-radius:4px;moz-border-radius:4px;khtml-border-radius:
+4px;o-border-radius:4px;webkit-border-radius:4px;ms-border-radius:4px
+;border: 0;width:24px; height:24px;" width="24" height="24" src="https://s3.amazonaws.com/images.wisestamp.com/icons_32/facebook.
+png"></a>&nbsp;<a href="http://twitter.com/AcademyLearning" target="_blank"><img moz-do-not-send="true" style="border-radius:4px;moz-border-radius:4px;khtml-border-radius:
+4px;o-border-radius:4px;webkit-border-radius:4px;ms-border-radius:4px
+;border: 0;width:24px; height:24px;" width="24" height="24" src="https://s3.amazonaws.com/images.wisestamp.com/icons_32/twitter.
+png"></a>&nbsp;<a href="http://www.youtube.com/user/AcademyofLearning09/" target="_blank"><img moz-do-not-send="true"
+                                            style="border-radius:4px;moz-border-radius:4px;khtml-border-radius:
+4px;o-border-radius:4px;webkit-border-radius:4px;ms-border-radius:4px
+;border: 0;width:24px; height:24px;" width="24" height="24" src="https://s3.amazonaws.com/images.wisestamp.com/icons_32/youtube.
+png"></a></div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div style="clear:both;height:0!important"></div>
+            <div class="wisestamp_app
+disclaimer" style="max-width: 600px; margin: 8px 8px 0 0; color:
+gray; border-top: 1px solid gray; font-size: 13px">
+                <div>IMPORTANT:
+                    The contents of this email and any attachments are confidential. They
+                    are<br> intended for the named recipient(s) only. If you have received
+                    this email by mistake,<br> please notify the sender immediately and do
+                    not disclose the contents to anyone or make<br> copies
+                    thereof.</div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
