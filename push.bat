105a9042b844dd6cb6a083f8deace28f04ea666d
MikeRoss <mike@aolccbc.com>
2022-04-07 12:41:56 -0700

    Update

--- push.bat
+++ push.bat
@@ -1,5 +1,5 @@
-del www/programs/images/compiled.html
-copy www/programs/images/*.html www/programs/images/compiled.html
+del www/programs/compiled.html
+copy www/programs/images/*.html www/programs/compiled.html
 git add .
 git commit -m "Update"
 git push
