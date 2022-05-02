a7cebc73d3a03f6a8484ac5475a71271533b0712
MikeRoss <mike@aolccbc.com>
2022-05-02 16:47:40 -0700

    sigh

--- aolccbc.com/pages/_app.js
+++ aolccbc.com/pages/_app.js
@@ -1,4 +1,8 @@
 import '../styles/globals.css'
+import styles from '../styles/Home.module.css'
+//import "@fortawesome/fontawesome-free/css/all.min.css";
+import 'bootstrap-css-only/css/bootstrap.min.css';
+//import 'mdbreact/dist/css/mdb.css';
 
 
 function MyApp({ Component, pageProps }) {
