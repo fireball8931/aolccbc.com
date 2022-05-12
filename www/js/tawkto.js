68155909659260a02359b15e90fba95d08b3e9d1
Michael Ross <mike@aolccbc.com>
2022-05-12 05:59:53 -0700

    add pug

---
+++ www/js/tawkto.js
@@ -0,0 +1,12 @@
+let tawktodiv = `<div id='tawk_5ac3f032d7591465c70929a6'></div>`
+var div2 = document.createElement("div")
+div2.insertBefore = tawktodiv
+
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date(); Tawk_API.embedded='tawk_5ac3f032d7591465c70929a6';
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5ac3f032d7591465c70929a6/1fu9tt2vr';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);})();
