beb8ad49eb23a6e3c1e8900ff47d86af14c9f461
Michael Ross <mike@aolccbc.com>
2022-11-04 08:21:21 -0700

    init

--- www/sexual-harassment/index.php
+++ www/sexual-harassment/index.php
@@ -2,7 +2,7 @@
 
 <head>
   <meta charset="utf-">
-  <title>Campus Policies</title><script src="/js/includes.js"></script><div w3-include-html="/css.html"></div>
+  <title>Campus Policies</title><script src="/www/js/includes.js"></script><div w3-include-html="/css.html"></div>
 </head>
 <body>
   <div w3-include-html="/navbar.html">
