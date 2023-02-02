97d434d1b1d3ebce75f221df8de40e471337cf16
Michael Ross <mike@aolccbc.com>
2023-02-02 12:45:50 -0800

    new js

---
+++ js/genEAlink.js
@@ -0,0 +1,22 @@
+function generateLink() {
+ let link="https://www.enrichedacademy.com/users/autologin?";
+ let token = "Vsv98l7AxT3o";
+ let suffix = "&indicator=0&host=aolccbc.com";
+//  "https://www.enrichedacademy.com/users/autologin?token=Vsv98l7AxT3o&fname=first_name_value&lname=last_name_value&email=email_address&indicator=0&host=aolccbc.com"
+
+let query = window.location.search.substring(1);
+let params = query.split('&');
+let result = {};
+
+params.forEach(param => {
+  let [key, value] = param.split('=');
+  result[key] = value;
+});
+
+let finalURL = `${link}${token}&fname=${result[fname]}&lname=${result[lname]}&email=${result[email]}${suffix}`;
+
+return finalURL
+
+}
+
+
