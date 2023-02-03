b34efc49c9ebccb1b92544165b75401ba3b6c7fd
Michael Ross <mike@aolccbc.com>
2023-02-03 00:42:37 +0000

    Enriched Academy start page

--- js/genEAlink.js
+++ js/genEAlink.js
@@ -1,10 +1,12 @@
-function generateLink() {
+function generateLink(query) {
  let link="https://www.enrichedacademy.com/users/autologin?";
  let token = "Vsv98l7AxT3o";
  let suffix = "&indicator=0&host=aolccbc.com";
 //  "https://www.enrichedacademy.com/users/autologin?token=Vsv98l7AxT3o&fname=first_name_value&lname=last_name_value&email=email_address&indicator=0&host=aolccbc.com"
 
-let query = window.location.search.substring(1);
+// let query = window.location.search.substring(1);
+
+
 let params = query.split('&');
 let result = {};
 
@@ -12,8 +14,9 @@ params.forEach(param => {
   let [key, value] = param.split('=');
   result[key] = value;
 });
+console.log(result)
 
-let finalURL = `${link}${token}&fname=${result[fname]}&lname=${result[lname]}&email=${result[email]}${suffix}`;
+let finalURL = `${link}${token}&fname=${result.fname}&lname=${result.lname}&email=${result.email}${suffix}`;
 
 return finalURL
 
