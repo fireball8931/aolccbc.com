1e19d20bd713b9baac304fad5cdcfe479a8c68d0
Michael Ross <mike@aolccbc.com>
2023-02-16 10:36:41 -0800

    Fix tt page

--- js/currency.js
+++ js/currency.js
@@ -1,3 +1,21 @@
+// TODO
+// $('.my-class div').each(function () {
+//     var $this = $(this);
+//     var item = $this.text();
+//     var num = Number(item).toLocaleString('en');    
+
+//     if (Number(item) < 0) {
+//         num = num.replace('-','');
+//         $this.addClass('negMoney');
+//     } else {
+//         $this.addClass('enMoney');
+//     }
+    
+//     $this.text(num);
+// });
+
+
+
 $('* div').each(function () {
     var item = $(this).text();
     var num = Number(item).toLocaleString('en');    
