221c0e267bf172b601b224a19afe958b18a36076
Mike <mike.ross@miross.net>
2023-02-16 06:18:03 -0800

    Update genEAlink.js
    ChatGPT optimized

--- js/genEAlink.js
+++ js/genEAlink.js
@@ -1,72 +1,36 @@
-//  TODO
-//  [x] Update code to use multiple API keys
-//  [x] Update Query URL to specify campus and match campus to API key
+const langleyKey = "ENPycLbv18g8";
+const abbotsfordKey = "Vsv98l7AxT3o";
 
-var langleykey = "ENPycLbv18g8"
-var abbotsfordkey = "Vsv98l7AxT3o"
 function generateLink(query) {
- let link="https://www.enrichedacademy.com/users/autologin?token=";
-//  let token = "Vsv98l7AxT3o";
- let suffix = "&indicator=0&host=aolccbc.com";
-//  "https://www.enrichedacademy.com/users/autologin?token=Vsv98l7AxT3o&fname=first_name_value&lname=last_name_value&email=email_address&indicator=0&host=aolccbc.com"
+  const link = "https://www.enrichedacademy.com/users/autologin?token=";
+  const suffix = "&indicator=0&host=aolccbc.com";
 
-// let query = window.location.search.substring(1);
+  const params = query.split('&').reduce((result, param) => {
+    const [key, value] = param.split('=');
+    result[key] = value;
+    return result;
+  }, {});
 
+  const token = params.campus === 'abbotsford' ? abbotsfordKey : langleyKey;
 
-let params = query.split('&');
-let result = {};
-
-params.forEach(param => {
-  let [key, value] = param.split('=');
-  result[key] = value;
-});
-console.log(result)
-if (result.campus === 'abbotsford') {
-  var token = abbotsfordkey
-} else {
-  var token = langleykey
+  const finalURL = link + token + "&fname=" + params.fname + "&lname=" + params.lname + "&email=" + params.email + suffix;
+  return finalURL;
 }
 
+function generateEmail() {
+  const fname = document.getElementById('fname').value;
+  const lname = document.getElementById('lname').value;
+  const email = document.getElementById('email').value;
+  const campus = document.getElementById('campus').value;
 
-let finalURL = `${link}${token}&fname=${result.fname}&lname=${result.lname}&email=${result.email}${suffix}`;
-
-return finalURL
-
+  const page = window.location.hostname === 'aolccbc.com' ? 'ea' : 'ea.html';
+  const genLink = window.location.protocol + '//' + window.location.hostname + '/' + page + '?fname=' + fname + '&lname=' + lname + '&email=' + email + '&campus=' + campus;
+  const emailBody = `Hello ${fname},\n\nAs part of your Thought Patterns for a Successful career, you have been invited to the Enriched Academy course. Click this link to access the course: ${genLink}\n\nPlease watch the orientation videos and then click the link on the page to start your course.\n\nThis course must be completed as well as the Thought Patterns course in myAOLCC in 20 hours of study.\n\nPlease let us know if you have issues accessing the course.`;
+  document.getElementById('emailcontent').textContent = emailBody;
 }
 
-
-function generateEmail(){
-  document.getElementById('emailcontent').innerHTML = (generateEmailfunc())
-}
-
-function generateEmailfunc(){
-  var fname = document.getElementById('fname').value //First Name
-  var lname = document.getElementById('lname').value //Last Name
-  var email = document.getElementById('email').value //Personal Email
-  var campus = document.getElementById('campus').value //Campus to be Associated with
-  if (window.location.hostname === 'aolccbc.com'){ 
-    var page = 'ea'
-  } else {
-    var page = 'ea.html'
-  }
-
-  let genLINK = `${window.location.protocol}//${window.location.hostname}/${page}?fname=${fname}&lname=${lname}&email=${email}&campus=${campus}`
-  let emailBody = `Hello ${fname},<br />
-As part of your Thought Patterns for a Successful career, you have been invited to the Enriched Academy course.<br/>--&gt; <a href="${genLINK}">Please click this link here </a>  &lt;--, 
-Please watch the orientation videos and then click the link on the page to start your course. <br />
-This course must be completed as well as the Thought Patterns course in myAOLCC in 20 hours of study.<br />
-Please let us know if you have issues accessing the course.`
-  return emailBody
-}
-
-function copyEmailHTML(){
-  // let copyText = document.getElementById("emailcontent");
-  // copyText.select();
-  // copyText.setSelectionRange(0, 99999);
-  var fname = document.getElementById('fname').value
-  var lname = document.getElementById('lname').value
-  var email = document.getElementById('email').value
-  
-  navigator.clipboard.writeText(generateEmailfunc());
+function copyEmailContentToClipboard() {
+  const emailContent = generateEmail();
+  navigator.clipboard.writeText(emailContent);
   alert("Copied the code, now compose a message to the student in Campus Login, go into source mode and paste at the top of the email. You can click on source again to see how the email looks");
 }
