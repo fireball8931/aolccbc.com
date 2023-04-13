057659e23f6be871c15d780c73967cf5ec0cc8bd
mike <mike@aolccbc.com>
2023-04-13 08:45:22 -0700

    work on fixing build

--- js/jsmaster.js
+++ js/jsmaster.js
@@ -1,4 +1,7 @@
-import('./popper.min.js')
-import('./jquery-3.5.1.min.js')
+// import * from '@popperjs/core';
+
+// import('./popper.min.js')
+// import('https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.9.2/umd/popper.min.js')
+// import('https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js')
 import('./gtag.js')
-import('./includes.js')
+// import('./includes.js')
