dba41bf1dc06ed1141dd7d17f35f34240afbb351
mike <mike@aolccbc.com>
2023-04-25 11:27:35 -0700

    refactor: Refactor code for better compatibility and performance
    - Refactor js/genEAlink.js for improved readability and compatibility
    - Switch from const to var for function parameters and variables
    - Concatenate URL components using + instead of +=
    - Replace textContent with value for improved browser compatibility
    - Minor updates and fixes for better code organization and efficiency

--- js/genEAlink.js
+++ js/genEAlink.js
@@ -1,36 +1,36 @@
-const langleyKey = "ENPycLbv18g8";
-const abbotsfordKey = "Vsv98l7AxT3o";
+var langleyKey = "ENPycLbv18g8";
+var abbotsfordKey = "Vsv98l7AxT3o";
 
 function generateLink(query) {
-  const link = "https://www.enrichedacademy.com/users/autologin?token=";
-  const suffix = "&indicator=0&host=aolccbc.com";
+  var link = "https://www.enrichedacademy.com/users/autologin?token=";
+  var suffix = "&indicator=0&host=aolccbc.com";
 
-  const params = query.split('&').reduce((result, param) => {
-    const [key, value] = param.split('=');
+  var params = query.split('&').reduce((result, param) => {
+    var [key, value] = param.split('=');
     result[key] = value;
     return result;
   }, {});
 
-  const token = params.campus === 'abbotsford' ? abbotsfordKey : langleyKey;
+  var token = params.campus === 'abbotsford' ? abbotsfordKey : langleyKey;
 
-  const finalURL = link + token + "&fname=" + params.fname + "&lname=" + params.lname + "&email=" + params.email + suffix;
+  var finalURL = link + token + "&fname=" + params.fname + "&lname=" + params.lname + "&email=" + params.email + suffix;
   return finalURL;
 }
 
 function generateEmail() {
-  const fname = document.getElementById('fname').value;
-  const lname = document.getElementById('lname').value;
-  const email = document.getElementById('email').value;
-  const campus = document.getElementById('campus').value;
+  var fname = document.getElementById('fname').value;
+  var lname = document.getElementById('lname').value;
+  var email = document.getElementById('email').value;
+  var campus = document.getElementById('campus').value;
 
-  const page = window.location.hostname === 'aolccbc.com' ? 'ea' : 'ea.html';
-  const genLink = window.location.protocol + '//' + window.location.hostname + '/' + page + '?fname=' + fname + '&lname=' + lname + '&email=' + email + '&campus=' + campus;
-  const emailBody = `Hello ${fname},\n\nAs part of your Thought Patterns for a Successful career, you have been invited to the Enriched Academy course. Click this link to access the course: ${genLink}\n\nPlease watch the orientation videos and then click the link on the page to start your course.\n\nThis course must be completed as well as the Thought Patterns course in myAOLCC in 20 hours of study.\n\nPlease let us know if you have issues accessing the course.`;
+  var page = window.location.hostname === 'aolccbc.com' ? 'ea' : 'ea.html';
+  var genLink = window.location.protocol + '//' + window.location.hostname + '/' + page + '?fname=' + fname + '&lname=' + lname + '&email=' + email + '&campus=' + campus;
+  var emailBody = `Hello ${fname},\n\nAs part of your Thought Patterns for a Successful career, you have been invited to the Enriched Academy course. Click this link to access the course: ${genLink}\n\nPlease watch the orientation videos and then click the link on the page to start your course.\n\nThis course must be completed as well as the Thought Patterns course in myAOLCC in 20 hours of study.\n\nPlease let us know if you have issues accessing the course.`;
   document.getElementById('emailcontent').textContent = emailBody;
 }
 
 function copyEmailContentToClipboard() {
-  const emailContent = generateEmail();
+  var emailContent = generateEmail();
   navigator.clipboard.writeText(emailContent);
   alert("Copied the code, now compose a message to the student in Campus Login, go into source mode and paste at the top of the email. You can click on source again to see how the email looks");
 }
